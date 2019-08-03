bc816e1974582b022c90a5c884d56b57e88fde2e
Tyler Holloway <tiloooor@Tylers-MacBook-Pro.local>
2019-08-02 20:15:28 -0400

    Added arrows to horizontal scroll menu

--- src/components/posts/GroupItem.js
+++ src/components/posts/GroupItem.js
@@ -26,8 +26,22 @@ const GroupItem = ({ profiles }) => {
             );
         });
 
+    const Arrow = ({ text, className }) => {
+        return (
+            <div
+                className={className}
+            >{text}</div>
+        );
+    };
+
+    const ArrowLeft = Arrow({ text: '<', className: 'arrow-prev' });
+    const ArrowRight = Arrow({ text: '>', className: 'arrow-next' });
     const menu = Menu();
-    return <ScrollMenu data={menu} />;
+
+    return <ScrollMenu
+        data={menu}
+        arrowLeft={ArrowLeft}
+        arrowRight={ArrowRight} />;
 };
 
 export default GroupItem;
--- src/components/profile/ViewedResources.js
+++ src/components/profile/ViewedResources.js
@@ -23,8 +23,22 @@ const ViewedResources = () => {
       return <ResourceItem resource={resource} />;
     });
 
+  const Arrow = ({ text, className }) => {
+    return (
+      <div
+        className={className}
+      >{text}</div>
+    );
+  };
+
+  const ArrowLeft = Arrow({ text: '<', className: 'arrow-prev' });
+  const ArrowRight = Arrow({ text: '>', className: 'arrow-next' });
   const menu = Menu();
-  return <ScrollMenu data={menu} />;
+
+  return <ScrollMenu
+        data={menu}
+        arrowLeft={ArrowLeft}
+        arrowRight={ArrowRight} />;
 };
 
 export default ViewedResources;
